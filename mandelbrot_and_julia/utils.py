c553067dd563b69e7786335ac1421675f6e936d6
Grégory Leventoux <gregory.leventoux@hotmail.fr>
2022-10-28 15:16:02 +0200

    modify plot_julia

--- mandelbrot_and_julia/utils.py
+++ mandelbrot_and_julia/utils.py
@@ -102,9 +102,9 @@ def plot_mandelbrot(zmin=-2-1j, zmax=1+1j, pixel_size=1e-2, max_iter=20, figname
         xmax, ymax = zmax.real, zmax.imag 
         x = np.arange(xmin+pixel_size/2, xmax+pixel_size/2, pixel_size)
         y = np.arange(ymax-pixel_size/2, ymin-pixel_size/2, -pixel_size)
-        z = x[np.newaxis,:] + y[:,np.newaxis] * 1j
+        c = x[np.newaxis,:] + y[:,np.newaxis] * 1j
         is_in_mandelbrot_vec = np.vectorize(is_in_mandelbrot)
-        grid = is_in_mandelbrot_vec(z,max_iter)
+        grid = is_in_mandelbrot_vec(c,max_iter)
         im = Image.fromarray(np.invert(grid))
         im.save('output/'+figname)
 
@@ -135,18 +135,12 @@ def plot_julia(c=0.8+0.156j, zmin=-1-1j, zmax=1+1j, pixel_size=1e-2, max_iter=20
     if width <= 0 or height <= 0:
         print("Window empty !")
     else:
-        nx = int(width/pixel_size)
-        ny = int(height/pixel_size)
-        a = np.full((ny,nx),True)
-        z0 = complex(zmin.real,zmax.imag)
-        dx = width/nx
-        dy = height/ny
-        for i in range(ny):
-            for j in range(nx):
-                z = z0 + complex((j+0.5)*dx,0) - complex(0,(i+0.5)*dy)
-                if is_in_julia(z,c,max_iter=max_iter):
-                    a[i][j] = False
-        im = Image.fromarray(a)
-
-        # set save file's path
+        xmin, ymin = zmin.real, zmin.imag
+        xmax, ymax = zmax.real, zmax.imag 
+        x = np.arange(xmin+pixel_size/2, xmax+pixel_size/2, pixel_size)
+        y = np.arange(ymax-pixel_size/2, ymin-pixel_size/2, -pixel_size)
+        z = x[np.newaxis,:] + y[:,np.newaxis] * 1j
+        is_in_julia_vec = np.vectorize(is_in_julia)
+        grid = is_in_julia_vec(z,c,max_iter)
+        im = Image.fromarray(np.invert(grid))
         im.save('output/'+figname)
